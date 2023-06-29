24ba3629537c2530979e3cc298a17609cee3fabd
김영현 <agilecatch@gmail.com>
2023-06-29 16:46:38 +0900

    좋아요버튼 구현

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,7 @@ class SearchPage extends StatelessWidget {
             toolbarHeight: 80,
             title: TextField(
               onSubmitted: (value) {
-                print(value);
+                bookService.search(value);
               },
               cursorColor: Colors.grey,
               decoration: InputDecoration(
@@ -112,30 +112,53 @@ class SearchPage extends StatelessWidget {
               itemBuilder: (context, index) {
                 if (bookService.bookList.isEmpty) return SizedBox();
                 Book book = bookService.bookList.elementAt(index);
-                return ListTile(
-                  onTap: () {},
-                  leading: Image.network(
-                    book.thumbnail,
-                    fit: BoxFit.fitHeight,
-                  ),
-                  title: Text(
-                    book.title,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  subtitle: Text(
-                    book.subtitle,
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  trailing: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.star_border),
-                  ),
-                );
+                return BookTile(book: book);
               },
             ),
           ),
         );
       },
+    );
+  }
+}
+
+class BookTile extends StatelessWidget {
+  const BookTile({
+    super.key,
+    required this.book,
+  });
+
+  final Book book;
+
+  @override
+  Widget build(BuildContext context) {
+    BookService bookService = context.read<BookService>();
+
+    return ListTile(
+      onTap: () {},
+      leading: Image.network(
+        book.thumbnail,
+        fit: BoxFit.fitHeight,
+      ),
+      title: Text(
+        book.title,
+        style: TextStyle(fontSize: 16),
+      ),
+      subtitle: Text(
+        book.subtitle,
+        style: TextStyle(color: Colors.grey),
+      ),
+      trailing: IconButton(
+        onPressed: () {
+          bookService.toggleLikeBook(book: book);
+        },
+        icon: bookService.likedBookList.map((book) => book.id).contains(book.id)
+            ? Icon(
+                Icons.star,
+                color: Colors.amber,
+              )
+            : Icon(Icons.star_border),
+      ),
     );
   }
 }

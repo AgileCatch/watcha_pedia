9177f85f4636f1106d004c104161763567df9339
김영현 <agilecatch@gmail.com>
2023-06-29 16:32:40 +0900

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'book.dart';
 import 'book_service.dart';
 
 void main() {
@@ -77,7 +78,7 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<BookService>(
-      builder: (context, BookService, child) {
+      builder: (context, bookService, child) {
         return Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.white,
@@ -101,8 +102,20 @@ class SearchPage extends StatelessWidget {
               ),
             ),
           ),
-          body: Center(
-            child: Text("검색"),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: ListView.separated(
+              itemCount: bookService.bookList.length,
+              separatorBuilder: (context, index) {
+                return Divider();
+              },
+              itemBuilder: (context, index) {
+                Book book = bookService.bookList.elementAt(index);
+                return ListTile(
+                  title: Text(book.title),
+                );
+              },
+            ),
           ),
         );
       },
